dbc141bb87d387477e955f1f9aab6e5d216af3f1
Licoo <46717842+Licoo27@users.noreply.github.com>
2024-01-26 11:33:52 +0100

    Update penduv2.py

--- penduv2.py
+++ penduv2.py
@@ -1,18 +1,16 @@
-import random
+import random 
 
-correct_char = [
-    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
-    "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-]
+correct_char =  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+"O" ,"P", "Q", "R", "S", "T", "U", "V", "W" ,"X", "Y", "Z"]
 
-word_list = ["avant", "bebe", "ou", "abandon"]
+word_list = ["AVANT", "BEBE", "OU", "ABANDON"]
 
 
-def word_with_blanks(word_to_guess):
+def blanks(word_to_guess):
   blanks = list(word_to_guess)
   for i in range(len(word_to_guess)):
-    blanks[i] = "__"
-  print(' '.join(blanks))
+    blanks[i] = "_"
+  return ''.join(blanks)
 
 
 def guess_a_letter():
@@ -25,20 +23,37 @@ def guess_a_letter():
   return guessed_letter
 
 
-def replace_letter():
-  for char in guess_a_letter:
-    if dico[char] == "_":
-      if char == guessed_letter:
-        dico[char] = guessed_letter
-  return dico
+def replace_letter(blanks, word):
+  tries = 10
+  blank_list = list(blanks)
+  while (''.join(blank_list)) != word or tries != 0:
+    in_the_word = False
+    x = guess_a_letter()
+    for i in range(len(word)):
+      if x == word[i]:
+        blank_list[i] = x 
+        in_the_word = True
+    if in_the_word:
+      tries -= 1
+      print(f"Well done, you guessed a letter, you have {tries} tries left")
+    if not in_the_word: 
+      tries -= 1
+      print(f"Sorry, {x} is not in the word, you have {tries} tries left")
+  
+    print(''.join(blank_list))
+  print("Congratulations, you guessed the word!")
 
 
 def regle_jeux_du_pendu():
   print(
-      "Hello, thank you for activating our programm of the hangman game\n Here is the rules for the game :\n -you have 1à tries to find the word\n -You need to write the letters in capital letter \n -There is no ponctuation or accent \n Good luck"
+      "Hello, thank you for activating our programm of the hangman game\n Here is the rules for the game :\n -you have 10 tries to find the word\n -You need to write the letters in capital letter \n -There is no ponctuation or accent \n Good luck"
   )
 
 
 word_to_guess = random.choice(word_list)
+word_with_blanks = word_to_guess
 print(word_to_guess)
-word_with_blanks(word_to_guess)
+print(blanks(word_to_guess))
+
+replace_letter(blanks(word_to_guess), word_to_guess)
+
