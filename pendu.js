c7e62289b2b1237fb21792a7f84fbe7364c2899f
Licoo <46717842+Licoo27@users.noreply.github.com>
2024-01-31 09:50:57 +0100

    Update pendu.js

--- pendu.js
+++ pendu.js
@@ -1,87 +1,109 @@
-var mots = ["javascript", "pendu", "informatique", "programmation", "html", "css", "groupe"];
-var motATrouver = choisirMot();
+vavar mots = [];
+
+async function recupMots() {
+  return await fetch("pli07.txt")
+    .then((res) => res.text())
+    .then((text) => {
+      mots = text.split("\n");
+      motATrouver = choisirMot();
+      afficherMotCache();
+      afficherLettresDites();
+    })
+    .catch((e) => console.error(e));
+}
+
+recupMots();
+
+console.log(mots);
+
+
+var motATrouver = ""
 var lettresTrouvees = [];
+var nblettresTrouvees = 0;
 var lettresDites = [];
-var essaisMax = 7;
+var essaisMax = 9;
 var essais = 0;
+var max_segment = 9;
+var hangman = document.getElementsByClassName("hangman");
+var hangmanDiv = []
+for (var i = 1; i < max_segment + 1; i++) {
+  hangmanDiv.push(document.getElementsByClassName("hangman" + i)[0]);
+}
+
+for (var i = 0; i < hangmanDiv.length; i++) {
+  hangmanDiv[i].classList.add("hidden-div");
+}
+
+var tab = []
+
 
 function choisirMot() {
-    return mots[Math.floor(Math.random() * mots.length)];
+  return mots[Math.floor(Math.random() * mots.length)];
 }
 
 function afficherMotCache() {
-    var motCache = "";
-    for (var i = 0; i < motATrouver.length; i++) {
-        if (lettresTrouvees.includes(motATrouver[i])) {
-            motCache += motATrouver[i];
-        } else {
-            motCache += "_";
-        }
+  var motCache = "";
+  for (var i = 0; i < motATrouver.length; i++) {
+    if (lettresTrouvees.includes(motATrouver[i])) {
+      motCache += motATrouver[i];
+    } else {
+      motCache += "_";
     }
-    document.getElementById("word").textContent = motCache + " ( " + motATrouver.length + " lettres )";
+  }
+  document.getElementById("word").textContent = motCache + " ( " + motATrouver.length + " lettres )";
 }
 
 function afficherLettresDites() {
-    document.getElementById("letters").textContent = lettresDites.join(", ");
+  document.getElementById("letters").textContent = lettresDites.join(", ");
 }
 
 function afficherMessage(message) {
-    var essaisRestants = essaisMax - essais;
-    document.getElementById("message").textContent = message + " Essais restants : " + essaisRestants;
+  var essaisRestants = essaisMax - essais;
+  document.getElementById("message").textContent = message + " Essais restants : " + essaisRestants;
 }
 
 function jouerTour() {
-    var lettreInput = document.getElementById("letter-input");
-    var lettre = lettreInput.value.toLowerCase();
+  var lettreInput = document.getElementById("letter-input");
+  var lettre = lettreInput.value.toUpperCase();
 
-    
-    if (!/^[a-zA-Z]$/.test(lettre)) {
-        afficherMessage("Veuillez entrer une lettre valide.");
-        lettreInput.value = "";  
-        return;
-    }
 
-    if (lettresDites.includes(lettre)) {
-        afficherMessage("Vous avez déjà dit cette lettre. Essayez une autre.");
-        return;
-    }
 
-    lettresDites.push(lettre);
+  if (!/^[a-zA-Z]$/.test(lettre)) {
+    afficherMessage("Veuillez entrer une lettre valide.");
+    lettreInput.value = "";
+    return;
+  }
 
-    if (motATrouver.includes(lettre)) {
-        lettresTrouvees.push(lettre);
-        afficherMessage("Bien joué !");
-    } else {
-        essais++;
-        afficherMessage("Raté... Essayez encore.");
-        document.getElementsByClassName("hangman").src = "hangman" + essais;
-    }
+  if (lettresDites.includes(lettre)) {
+    afficherMessage("Vous avez déjà dit cette lettre. Essayez une autre.");
+    return;
+  }
 
-    afficherMotCache();
-    afficherLettresDites();
+  lettresDites.push(lettre);
 
-    if (lettresTrouvees.length === motATrouver.length) {
-        afficherMessage("Félicitations ! Vous avez trouvé le mot : " + motATrouver);
-    }
+  if (motATrouver.includes(lettre)) {
+    lettresTrouvees.push(lettre);
+    nblettresTrouvees += motATrouver.split(lettre).length - 1;
+    afficherMessage("Bien joué !");
+  } else {
+    essais++;
+    afficherMessage("Raté... Essayez encore.");
+    document.getElementsByClassName("hangman").src = "hangman" + essais;
+    hangmanDiv[essais - 1].classList.remove("hidden-div");
+  }
 
-    if (essais === essaisMax) {
-        afficherMessage("Dommage ! Vous avez épuisé tous vos essais. Le mot était : " + motATrouver);
-    }
+  afficherMotCache();
+  afficherLettresDites();
 
-    lettreInput.value = "";  
-}
+  if (nblettresTrouvees === motATrouver.length) {
+    afficherMessage("Félicitations ! Vous avez trouvé le mot : " + motATrouver);
+  }
 
-function recommencerPartie() {
-    motATrouver = choisirMot();
-    lettresTrouvees = [];
-    lettresDites = [];
-    essaisMax = 7;
-    essais = 0;
-    document.getElementById("hangman-image").src = "hangman0.png";
-    afficherMessage("");
-    afficherMotCache();
-    afficherLettresDites();
-    document.getElementById("letter-input").value = "";
+  if (essais === essaisMax) {
+    afficherMessage("PENDU! Le mot était : " + motATrouver);
+  }
+
+  lettreInput.value = "";
 }
 
 
